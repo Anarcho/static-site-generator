a1d553661e345b98e898a1bb72cc499c07fdb05b
anarcho <aaron.karlsen@gmail.com>
2024-10-27 10:36:02 +1000

    adding inline utils

--- src/test-scratch.py
+++ src/test-scratch.py
@@ -1,73 +1,26 @@
 from htmlnode import HTMLNode, LeafNode, ParentNode, TagType
 from textnode import TextNode, TextType
 
+string_node = "This is a text with a  **bold phrase** in the middle"
 
-node = ParentNode(
-    "p",
-    [
-        LeafNode("a", "click me!", {"href": "http://boot.dev"}),
-        LeafNode(None, "Normal Text"),
-        LeafNode("i", None),
-        LeafNode(None, "Normal"),
-    ],
-)
+node = TextNode(string_node, TextType.BOLD)
 
-
-node2 = ParentNode(
-    "p",
-    [
-        LeafNode("a", "click me!", {"href": "http://boot.dev"}),
-        LeafNode(None, "Normal Text"),
-        LeafNode("i", "Italic"),
-        LeafNode(None, "Normal"),
-        ParentNode("p", None, None),
-    ],
-    {"href": "http://boot.dev"},
-)
-
-node3 = ParentNode(
-    "p",
-    [
-        LeafNode("a", "click me!", {"href": "http://boot.dev"}),
-        LeafNode(None, "Normal Text"),
-        LeafNode("i", "Italic"),
-        LeafNode(None, "Normal"),
-        ParentNode("p", [LeafNode(None, "Normal")], {"href": "http://boot.dev"}),
-    ],
-    {"href": "http://boot.dev"},
-)
-
-tag = TagType.LINK.value
-value = "This is a html node"
-children = []
-props = {"href": "http://boot.dev"}
-
-node4 = HTMLNode(tag, value, children, props)
-
-node5 = ParentNode(
-    "p",
-    [
-        LeafNode("b", "Bold text"),
-        LeafNode(None, "Normal text"),
-        LeafNode("i", "italic text"),
-        LeafNode(None, "Normal text"),
-    ],
-)
-
-node6 = TextNode("This is bold", TagType.BOLD)
-
-try:
-    node.to_html()
-except Exception as e:
-    print(str(e))
+# older_nodes = [node]
+# delimiter = "**"
+# text_type = TextType
+#
+# TextNode = text, text_type, url
 
 
-try:
-    node2.to_html()
-except Exception as e:
-    print(str(e))
-print(node3.to_html())
+def split_nodes_delimiter(old_nodes, delimiter, text_type):
+    result = []
+    for node in old_nodes:
+        inner_result = []
+        if node.text.count(delimiter) != 2:
+            raise Exception("Invalid Markdown: delimiter not present in node")
+        elif text_type != TextType.TEXT 
 
-print(node5.props_to_html())
-print(node6.text_type)
+    return result 
 
+
+split_nodes_delimiter([node], "**", TextType.BOLD)
